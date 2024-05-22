18db4b5e2e43bcfed76b7831a4d0ee49419a5ad7
Star Rauchenberger <fefferburbia@gmail.com>
2024-05-22 09:25:08 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.NET.Common;
+
+namespace AnodyneArchipelago
+{
+    [BepInPlugin("com.fourisland.plugins.anodyne.archipelago", "Anodyne Archipelago", "1.0.0.0")]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
