4c6c9043813f5f61282dbd14e97847264bfddd67
Star Rauchenberger <fefferburbia@gmail.com>
2024-05-31 16:59:01 -0400

    Remove special behavior from Street chests

--- AnodyneArchipelago/ArchipelagoTreasure.cs
+++ AnodyneArchipelago/ArchipelagoTreasure.cs
@@ -90,23 +90,8 @@ namespace AnodyneArchipelago
 
         public override void GetTreasure()
         {
-            if (_location == "Street - Broom Chest")
-            {
-                BroomTreasure broomTreasure = new("broom-icon", this.Position, BroomType.Normal);
-                broomTreasure.GetTreasure();
-                GlobalState.SpawnEntity(broomTreasure);
-            }
-            else if (_location == "Street - Key Chest")
-            {
-                KeyTreasure keyTreasure = new(this.Position);
-                keyTreasure.GetTreasure();
-                GlobalState.SpawnEntity(keyTreasure);
-            }
-            else
-            {
-                base.GetTreasure();
-            }
-            
+            base.GetTreasure();
+
             Plugin.ArchipelagoManager.SendLocation(_location);
         }
     }
